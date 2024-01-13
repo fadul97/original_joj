d938a813706ab10b16be6b65603ec8e4f3ca3776
Leonardo Fadul <lsffadul@gmail.com>
2024-01-13 19:17:21 -0300

    X11Input class

---
+++ include/joj/platform/x11/input_x11.h
@@ -0,0 +1,26 @@
+#ifndef JOJ_X11_INPUT_H
+#define JOJ_X11_INPUT_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#if JPLATFORM_LINUX
+
+#include "platform/input.h"
+
+namespace joj
+{
+    class JAPI X11Input : public Input
+    {
+    public:
+        X11Input();
+        ~X11Input();
+        
+    private:
+        Keys translate_keycode(u32 keycode);
+    };
+}
+
+#endif // JPLATFORM_LINUX
+
+#endif // JOJ_X11_INPUT_H
