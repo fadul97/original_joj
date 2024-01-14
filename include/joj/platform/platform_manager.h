c1e2f16f28dd8be71e13a6967bbdd7ae60b2bff7
Leonardo Fadul <lsffadul@gmail.com>
2024-01-14 02:19:03 -0300

    Added key reading methods in Abstract PlatformManager class

--- include/joj/platform/platform_manager.h
+++ include/joj/platform/platform_manager.h
@@ -19,6 +19,14 @@ namespace joj
 
         b8 is_running() const;
 
+        b8 is_key_down(Keys key) const;
+        b8 is_key_pressed(Keys key) const;
+        b8 is_key_up(Keys key) const;
+
+        i16 get_xmouse() const;
+        i16 get_ymouse() const;
+        i16 get_mouse_wheel() const;
+
         void close_window();
 
         virtual b8 init(i16 width = 800, i16 height = 600, std::string title = std::string{ "Joj PlatformManager" }) = 0;
@@ -35,6 +43,24 @@ namespace joj
 
     inline b8 PlatformManager::is_running() const
     { return window->is_running(); }
+
+    inline b8 PlatformManager::is_key_down(Keys key) const
+    { return input->is_key_down(key); }
+
+    inline b8 PlatformManager::is_key_pressed(Keys key) const
+    { return input->is_key_pressed(key); }
+
+    inline b8 PlatformManager::is_key_up(Keys key) const
+    { return input->is_key_up(key); }
+
+    inline i16 PlatformManager::get_xmouse() const
+    { return input->get_xmouse(); }
+
+    inline i16 PlatformManager::get_ymouse() const
+    { return input->get_ymouse(); }
+
+    inline i16 PlatformManager::get_mouse_wheel() const
+    { return input->get_mouse_wheel(); }
 
     inline void PlatformManager::close_window()
     { window->close(); }
