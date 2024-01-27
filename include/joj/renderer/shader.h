2fff517b1a72312665d986ed1d2ab211ddb2a96f
Leonardo Fadul <lsffadul@gmail.com>
2024-01-27 17:13:55 -0300

    Changed Shader method prototypes

--- include/joj/renderer/shader.h
+++ include/joj/renderer/shader.h
@@ -17,11 +17,11 @@ namespace joj
         Shader(const char* vertex_path, const char* fragment_path);
         virtual ~Shader();
 
-        virtual b8 compile_shaders(const char* vertex, const char* fragment) = 0;
+        virtual b8 compile_shaders(const char* vertex_shader, const char* fragment_shader) = 0;
         virtual void use() const = 0;
-        virtual void set_vec3(const std::string name, const Vector3 v) = 0;
-        virtual void set_vec4(const std::string name, const Vector4 v) = 0;
-        virtual void set_mat4(const std::string name, const Matrix4 m) = 0;
+        virtual void set_vec3(const std::string name, const Vector3 v) const = 0;
+        virtual void set_vec4(const std::string name, const Vector4 v) const = 0;
+        virtual void set_mat4(const std::string name, const Matrix4 m) const = 0;
     };
 }
 #endif // JOJ_SHADER_H
