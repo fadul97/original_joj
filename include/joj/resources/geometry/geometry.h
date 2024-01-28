6f4ab7ae30bd6d544c0972d5e22555773813f608
Leonardo Fadul <lsffadul@gmail.com>
2024-01-28 17:02:42 -0300

    Resources folder and Geometry class

---
+++ include/joj/resources/geometry/geometry.h
@@ -0,0 +1,87 @@
+#ifndef JOJ_GEOMETRY_H
+#define JOJ_GEOMETRY_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#include "math/jmath.h"
+#include <vector>
+
+namespace joj
+{
+    enum class GeometryType { UNKNOWN, CUBE, CYLINDER, SPHERE, GEOSPHERE, GRID, QUAD };
+
+    struct Vertex
+    {
+        Vector3 pos;
+        Vector4 color;
+    };
+
+    class JAPI Geometry
+    {
+    public:
+        Geometry();
+        virtual ~Geometry();
+
+        std::vector<Vertex> vertices;
+        std::vector<u32> indices;
+
+        virtual f32 x() const;
+        virtual f32 y() const;
+        virtual f32 z() const;
+
+        virtual void translate(f32 dx, f32 dy, f32 dz);
+		virtual void move_to(f32 px, f32 py, f32 pz);
+
+        virtual Vector3 get_position() const;
+        virtual GeometryType get_type() const;
+
+        const Vertex* get_vertex_data() const;
+        const u32* get_index_data() const;
+
+        u32 get_vertex_count() const;
+        u32 get_index_count() const;
+
+    protected:
+        Vector3 position;
+        GeometryType type;
+
+        void subdivide();
+    };
+
+    
+    inline f32 Geometry::x() const
+    { return position.x; }
+    
+    inline f32 Geometry::y() const
+    { return position.y; }
+    
+    inline f32 Geometry::z() const
+    { return position.z; }
+    
+    inline void Geometry::translate(f32 dx, f32 dy, f32 dz)
+    { position.x += dx; position.y += dy; position.z += dz; }
+
+    inline void Geometry::move_to(f32 px, f32 py, f32 pz)
+    { position.x = px; position.y = py; position.z = pz; }
+
+    inline Vector3 Geometry::get_position() const
+    { return position; }
+    
+    inline GeometryType Geometry::get_type() const
+    { return type; }
+    
+    inline const Vertex* Geometry::get_vertex_data() const
+    { return vertices.data(); }
+    
+    inline const u32* Geometry::get_index_data() const
+    { return indices.data(); }
+    
+    inline u32 Geometry::get_vertex_count() const
+    { return static_cast<u32>(vertices.size()); }
+    
+    inline u32 Geometry::get_index_count() const
+    { return static_cast<u32>(indices.size()); }
+}
+
+#endif // JOJ_GEOMETRY_H
---
+++ src/resources/geometry/geometry.cpp
@@ -0,0 +1,69 @@
+#include "resources/geometry/geometry.h"
+
+joj::Geometry::Geometry()
+    : position(0.0f, 0.0f, 0.0f), type(GeometryType::UNKNOWN)
+{
+}
+
+joj::Geometry::~Geometry()
+{
+}
+
+void joj::Geometry::subdivide()
+{
+    // Save a copy of the original geometry
+    std::vector <Vertex> vertices_copy = vertices;
+    std::vector <u32> indices_copy = indices;
+
+    vertices.resize(0);
+    indices.resize(0);
+
+    //       v1
+    //       *
+    //      / \
+    //     /   \
+    //  m0*-----*m1
+    //   / \   / \
+    //  /   \ /   \
+    // *-----*-----*
+    // v0    m2     v2
+
+    u32 num_tris = static_cast<u32>(indices_copy.size() / 3);
+
+    for (u32 i = 0; i < num_tris; ++i)
+    {
+        Vertex v0 = vertices_copy[indices_copy[size_t(i) * 3 + 0]];
+        Vertex v1 = vertices_copy[indices_copy[size_t(i) * 3 + 1]];
+        Vertex v2 = vertices_copy[indices_copy[size_t(i) * 3 + 2]];
+
+        // Find center points of each edge
+        Vertex m0, m1, m2;
+        m0.pos = (v0.pos + v1.pos) * 0.5f;
+        m1.pos = (v1.pos + v2.pos) * 0.5f;
+        m2.pos = (v0.pos + v2.pos) * 0.5f;
+
+        // Add new geometry
+        vertices.push_back(v0); // 0
+        vertices.push_back(v1); // 1
+        vertices.push_back(v2); // 2
+        vertices.push_back(m0); // 3
+        vertices.push_back(m1); // 4
+        vertices.push_back(m2); // 5
+
+        indices.push_back(i * 6 + 0);
+        indices.push_back(i * 6 + 3);
+        indices.push_back(i * 6 + 5);
+
+        indices.push_back(i * 6 + 3);
+        indices.push_back(i * 6 + 4);
+        indices.push_back(i * 6 + 5);
+
+        indices.push_back(i * 6 + 5);
+        indices.push_back(i * 6 + 4);
+        indices.push_back(i * 6 + 2);
+
+        indices.push_back(i * 6 + 3);
+        indices.push_back(i * 6 + 1);
+        indices.push_back(i * 6 + 4);
+    }
+}
