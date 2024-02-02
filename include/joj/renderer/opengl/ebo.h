67b6eefc2924efbaf3d873dbec7e7327858f7f22
Leonardo Fadul <lsffadul@gmail.com>
2024-02-02 19:50:48 -0300

    EBO class

---
+++ include/joj/renderer/opengl/ebo.h
@@ -0,0 +1,29 @@
+#ifndef JOJ_GL_EBO_H
+#define JOJ_GL_EBO_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#if JPLATFORM_LINUX
+
+#include "graphics/x11/joj_gl_x11.h"
+
+namespace joj
+{
+    class JAPI EBO
+    {
+    public:
+        EBO();
+        ~EBO();
+
+        void bind() const;
+
+        void bind_buffer_data(GLsizeiptr size, const u32* data);
+
+        u32 id;
+    };
+}
+
+#endif // JPLATFORM_LINUX
+
+#endif // JOJ_GL_EBO_H
---
+++ src/renderer/opengl/ebo.cpp
@@ -0,0 +1,21 @@
+#include "renderer/opengl/ebo.h"
+
+joj::EBO::EBO()
+{
+    glGenBuffers(1, &id);
+}
+
+joj::EBO::~EBO()
+{
+}
+
+void joj::EBO::bind() const
+{
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
+}
+
+void joj::EBO::bind_buffer_data(GLsizeiptr size, const u32* data)
+{
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+}
+
