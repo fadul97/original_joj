34f9d93c1cedf460a2935c5d3216866f3e9689c4
Leonardo Fadul <lsffadul@gmail.com>
2024-02-02 19:52:54 -0300

    Basic VAO class

---
+++ include/joj/renderer/opengl/vao.h
@@ -0,0 +1,35 @@
+#ifndef JOJ_GL_VAO_H
+#define JOJ_GL_VAO_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#if JPLATFORM_LINUX
+
+#include "vbo.h"
+#include "ebo.h"
+#include "resources/geometry/geometry.h"
+
+namespace joj
+{
+    class JAPI VAO
+    {
+    public:
+        VAO();
+        ~VAO();
+
+        void bind() const;
+        void unbind() const;
+
+        void bind_buffer_data(GLsizeiptr vertex_size, const Vertex* vertex_data, GLsizeiptr index_size, const u32* index_data);
+        void bind_buffer_data(Geometry& geometry);
+
+        u32 id;
+        VBO vbo;
+        EBO ebo;
+    };
+}
+
+#endif // JPLATFORM_LINUX
+
+#endif // JOJ_GL_VAO_H
---
+++ src/renderer/opengl/vao.cpp
@@ -0,0 +1,64 @@
+#include "renderer/opengl/vao.h"
+#include "renderer/opengl/vbo.h"
+#include "resources/geometry/vertex.h"
+
+#if JPLATFORM_LINUX
+#include "graphics/x11/joj_gl_x11.h"
+#endif // JPLATFORM_LINUX
+
+joj::VAO::VAO()
+{
+    glGenVertexArrays(1, &id);
+    vbo = joj::VBO{};
+    ebo = joj::EBO{};
+}
+
+joj::VAO::~VAO()
+{
+}
+
+void joj::VAO::bind() const
+{
+    glBindVertexArray(id);
+}
+
+void joj::VAO::unbind() const
+{
+    glBindVertexArray(0);
+}
+
+void joj::VAO::bind_buffer_data(GLsizeiptr vertex_size, const Vertex* vertex_data, GLsizeiptr index_size, const u32* index_data)
+{
+    bind();
+    
+    vbo.bind();
+    vbo.bind_buffer_data(vertex_size, vertex_data);
+
+    ebo.bind();
+    ebo.bind_buffer_data(index_size, index_data);
+
+    vbo.specify_position_data(sizeof(Vertex), (void*)0);
+    vbo.specify_color_data(sizeof(Vertex), (void*)(3 * sizeof(f32)));
+
+    vbo.unbind();
+
+    unbind();
+}
+
+void joj::VAO::bind_buffer_data(Geometry& geometry)
+{
+    bind();
+
+    vbo.bind();
+    vbo.bind_buffer_data(geometry.get_vertex_count() * sizeof(Vertex), geometry.get_vertex_data());
+
+    ebo.bind();
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, geometry.get_index_count() * sizeof(u32), geometry.get_index_data(), GL_STATIC_DRAW);
+
+    vbo.specify_position_data(sizeof(Vertex), (void*)0);
+    vbo.specify_color_data(sizeof(Vertex), (void*)(3 * sizeof(f32)));
+
+    vbo.unbind();
+
+    unbind();
+}
