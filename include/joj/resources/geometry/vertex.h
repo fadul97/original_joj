9a358e03bd2184beab3540a74021b74ca0946d23
Leonardo Fadul <lsffadul@gmail.com>
2024-02-02 19:53:54 -0300

    Separated Vertex into its own file (for now)

---
+++ include/joj/resources/geometry/vertex.h
@@ -0,0 +1,16 @@
+#ifndef JOJ_VERTEX_H
+#define JOJ_VERTEX_H
+
+#include "math/jmath.h"
+
+namespace joj
+{
+    struct Vertex
+    {
+        Vector3 pos;
+        Vector4 color;
+    };
+}
+
+
+#endif // JOJ_VERTEX_H
