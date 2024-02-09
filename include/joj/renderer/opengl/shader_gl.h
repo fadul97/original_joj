51a563368985f4f243d228ee796cefc0f4ac2348
Leonardo Fadul <lsffadul@gmail.com>
2024-02-09 15:22:34 -0300

    set_float and set_vec4 methods in GLShader

--- include/joj/renderer/opengl/shader_gl.h
+++ include/joj/renderer/opengl/shader_gl.h
@@ -10,6 +10,8 @@
 #include "graphics/x11/joj_gl_x11.h"
 #endif // JPLATFORM_LINUX
 
+#include "texture_rect_gl.h"
+
 namespace joj
 {
     enum class ShaderType { VERTEX, FRAGMENT, PROGRAM };
@@ -24,7 +26,9 @@ namespace joj
         b8 compile_shaders(const char* vertex_shader, const char* fragment_shader) override;
         void use() const override;
         void set_int(const std::string name, const i32 value) const;
+        void set_float(const std::string name, const f32 value) const;
         void set_vec3(const std::string name, const Vector3 v) const override;
+        void set_vec4(const std::string name, const GLTextureRect r) const;
         void set_vec4(const std::string name, const Vector4 v) const override;
         void set_mat4(const std::string name, const Matrix4 m) const override;
         void destroy() override;
@@ -40,8 +44,14 @@ namespace joj
     inline void GLShader::set_int(const std::string name, const i32 value) const
     { glUniform1i(glGetUniformLocation(id, name.c_str()), value); }
 
+    inline void GLShader::set_float(const std::string name, const f32 value) const
+    { glUniform1f(glGetUniformLocation(id, name.c_str()), value); }
+
     inline void GLShader::set_vec3(const std::string name, const Vector3 v) const
     { glUniform3f(glGetUniformLocation(id, name.c_str()), v.x, v.y, v.z); }
+
+    inline void GLShader::set_vec4(const std::string name, const GLTextureRect r) const
+    { glUniform4f(glGetUniformLocation(id, name.c_str()), r.m_x0, r.m_y0, r.m_x1, r.m_y1); }
 
     inline void GLShader::set_vec4(const std::string name, const Vector4 v) const
     { glUniform4f(glGetUniformLocation(id, name.c_str()), v.x, v.y, v.z, v.w); }
