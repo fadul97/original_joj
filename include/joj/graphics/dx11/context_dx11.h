47121218db31257475879e82112e1e291b11f6a7
Leonardo Fadul <lsffadul@gmail.com>
2024-02-25 19:26:02 -0300

    Get methods in DX11Context class

--- include/joj/graphics/dx11/context_dx11.h
+++ include/joj/graphics/dx11/context_dx11.h
@@ -20,19 +20,37 @@ namespace joj
         DX11Context();
         ~DX11Context();
 
+        ID3D11Device* get_device() const;
+        ID3D11DeviceContext* get_device_context() const;
+
+        IDXGIFactory6* get_factory() const;
+        ID3D11Debug* get_debug() const;
+
         b8 create(std::unique_ptr<Win32Window>& window) override;
         void make_current(std::unique_ptr<Win32Window>& window) override;
         void destroy() override;
           
     private:  
         ID3D11Device* m_device;
-        ID3D11DeviceContext* m_context;
+        ID3D11DeviceContext* m_device_context;
 
         IDXGIFactory6* m_factory;
         ID3D11Debug* m_debug;
 
         void log_hardware_info() override;
     };
+    
+    inline ID3D11Device* DX11Context::get_device() const
+    { return m_device; }
+    
+    inline ID3D11DeviceContext* DX11Context::get_device_context() const
+    { return m_device_context; }
+
+    inline IDXGIFactory6* DX11Context::get_factory() const
+    { return m_factory; }
+
+    inline ID3D11Debug* DX11Context::get_debug() const
+    { return m_debug; }
 }
 
 #endif // JPLATFORM_WINDOWS
