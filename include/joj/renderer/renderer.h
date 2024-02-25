5ee6a17366bcb659436df6a42916a3694158a20b
Leonardo Fadul <lsffadul@gmail.com>
2024-02-25 19:24:58 -0300

    New swap_buffers() method in Renderer class

--- include/joj/renderer/renderer.h
+++ include/joj/renderer/renderer.h
@@ -19,6 +19,7 @@ namespace joj
         virtual b8 init(std::unique_ptr<Twindow>& window) = 0;
         virtual void render() = 0;
         virtual void clear(f32 r = 0.0f, f32 g = 0.0f, f32 b = 0.0f, f32 a = 0.0f) = 0;
+        virtual void swap_buffers() = 0;
         virtual void shutdown() = 0;
     };
 }
