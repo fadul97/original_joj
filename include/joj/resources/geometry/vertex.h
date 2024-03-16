2652d26569beeb79a96017fb41cd1055add8c56f
Leonardo Fadul <lsffadul@gmail.com>
2024-03-16 18:04:52 -0300

    Using DirectX library on Windows - TODO: check Linux build

--- include/joj/resources/geometry/vertex.h
+++ include/joj/resources/geometry/vertex.h
@@ -1,7 +1,17 @@
 #ifndef JOJ_VERTEX_H
 #define JOJ_VERTEX_H
 
+#if JPLATFORM_WINDOWS
+#include <DirectXMath.h>
+namespace joj
+{
+#define J_PI DirectX::XM_PI
+    using Vector3 = DirectX::XMFLOAT3;
+    using Vector4 = DirectX::XMFLOAT4;
+}
+#else
 #include "math/jmath.h"
+#endif
 
 namespace joj
 {
--- src/resources/geometry/geo_sphere.cpp
+++ src/resources/geometry/geo_sphere.cpp
@@ -1,13 +1,18 @@
 #include "resources/geometry/geo_sphere.h"
-#include "jmath.h"
 
 joj::GeoSphere::GeoSphere()
 {
     type = GeometryType::GEOSPHERE;
 }
 
+#if JPLATFORM_WINDOWS
+#include <DirectXColors.h>
+
+using namespace DirectX;
+
 joj::GeoSphere::GeoSphere(f32 radius, u32 subdivisions)
 {
+
     type = GeometryType::GEOSPHERE;
 
     // Max number of subdivisions is 6
@@ -52,12 +57,13 @@ joj::GeoSphere::GeoSphere(f32 radius, u32 subdivisions)
     for (u32 i = 0; i < vertices.size(); ++i)
     {
         // Normalize vetor (point)
-        Vector3 n = normalize(vertices[i].pos);
+        DirectX::XMVECTOR n = DirectX::XMVector3Normalize(XMLoadFloat3(&vertices[i].pos));
 
         // Project on sphere
-        vertices[i].pos = n * radius;
+        DirectX::XMVECTOR p = radius * n;
 
-        vertices[i].color = Vector4{1.0f, 1.0f, 0.0f, 1.0f};
+        XMStoreFloat3(&vertices[i].pos, p);
+        vertices[i].color = DirectX::XMFLOAT4(DirectX::Colors::Yellow);
     }
 }
 
@@ -83,7 +89,121 @@ joj::GeoSphere::GeoSphere(f32 radius, u32 subdivisions, Vector4 color)
         Vector3{Z, -X, 0.0f},  Vector3{-Z, -X, 0.0f}
     };
 
-     // Indices of the icosahedron
+    // Indices of the icosahedron
+    u32 k[60] =
+    {
+        1,4,0,  4,9,0,  4,5,9,  8,5,4,  1,8,4,
+        1,10,8, 10,3,8, 8,3,5,  3,2,5,  3,7,2,
+        3,10,7, 10,6,7, 6,11,7, 6,0,11, 6,1,0,
+        10,1,6, 11,0,9, 2,11,9, 5,2,9,  11,2,7
+    };
+
+    // Snap and initialize vertex vectors and indices
+    vertices.resize(12);
+    indices.assign(&k[0], &k[60]);
+
+    for (u32 i = 0; i < 12; ++i)
+        vertices[i].pos = pos[i];
+
+    // Subdivide each triangle of the icosahedron a certain number of times
+    for (u32 i = 0; i < subdivisions; ++i)
+        subdivide();
+
+    // Project the vertices onto a sphere and adjust the scale
+    for (u32 i = 0; i < vertices.size(); ++i)
+    {
+        // Normalize vetor (point)
+        DirectX::XMVECTOR n = DirectX::XMVector3Normalize(XMLoadFloat3(&vertices[i].pos));
+
+        // Project on sphere
+        DirectX::XMVECTOR p = radius * n;
+
+        XMStoreFloat3(&vertices[i].pos, p);
+
+        vertices[i].color = color;
+    }
+}
+
+#else
+joj::GeoSphere::GeoSphere(f32 radius, u32 subdivisions)
+{
+
+    type = GeometryType::GEOSPHERE;
+
+    // Max number of subdivisions is 6
+    subdivisions = (subdivisions > 6U ? 6U : subdivisions);
+
+    // Approximate a sphere by the subdivision of an icosahedron
+    const f32 X = 0.525731f;
+    const f32 Z = 0.850651f;
+
+    // Vertices of the icosahedron
+    Vector3 pos[12] =
+    {
+        Vector3{-X, 0.0f, Z},  Vector3{X, 0.0f, Z},
+        Vector3{-X, 0.0f, -Z}, Vector3{X, 0.0f, -Z},
+        Vector3{0.0f, Z, X},   Vector3{0.0f, Z, -X},
+        Vector3{0.0f, -Z, X},  Vector3{0.0f, -Z, -X},
+        Vector3{Z, X, 0.0f},   Vector3{-Z, X, 0.0f},
+        Vector3{Z, -X, 0.0f},  Vector3{-Z, -X, 0.0f}
+    };
+
+    // Indices of the icosahedron
+    u32 k[60] =
+    {
+        1,4,0,  4,9,0,  4,5,9,  8,5,4,  1,8,4,
+        1,10,8, 10,3,8, 8,3,5,  3,2,5,  3,7,2,
+        3,10,7, 10,6,7, 6,11,7, 6,0,11, 6,1,0,
+        10,1,6, 11,0,9, 2,11,9, 5,2,9,  11,2,7
+    };
+
+    // Snap and initialize vertex vectors and indices
+    vertices.resize(12);
+    indices.assign(&k[0], &k[60]);
+
+    for (u32 i = 0; i < 12; ++i)
+        vertices[i].pos = pos[i];
+
+    // Subdivide each triangle of the icosahedron a certain number of times
+    for (u32 i = 0; i < subdivisions; ++i)
+        subdivide();
+
+    // Project the vertices onto a sphere and adjust the scale
+    for (u32 i = 0; i < vertices.size(); ++i)
+    {
+        // Normalize vetor (point)
+        Vector3 n = normalize(vertices[i].pos);
+
+        // Project on sphere
+        vertices[i].pos = n * radius;
+
+        vertices[i].color = Vector4{ 1.0f, 1.0f, 0.0f, 1.0f };
+    }
+}
+
+joj::GeoSphere::GeoSphere(f32 radius, u32 subdivisions, Vector4 color)
+{
+    type = GeometryType::GEOSPHERE;
+
+    // Max number of subdivisions is 6
+    subdivisions = (subdivisions > 6U ? 6U : subdivisions);
+
+    // Approximate a sphere by the subdivision of an icosahedron
+    const f32 X = 0.525731f;
+    const f32 Z = 0.850651f;
+
+    // Vertices of the icosahedron
+    Vector3 pos[12] =
+    {
+        Vector3{-X, 0.0f, Z},  Vector3{X, 0.0f, Z},
+        Vector3{-X, 0.0f, -Z}, Vector3{X, 0.0f, -Z},
+        Vector3{0.0f, Z, X},   Vector3{0.0f, Z, -X},
+        Vector3{0.0f, -Z, X},  Vector3{0.0f, -Z, -X},
+        Vector3{Z, X, 0.0f},   Vector3{-Z, X, 0.0f},
+        Vector3{Z, -X, 0.0f},  Vector3{-Z, -X, 0.0f}
+    };
+
+    // Indices of the icosahedron
     u32 k[60] =
     {
         1,4,0,  4,9,0,  4,5,9,  8,5,4,  1,8,4,
@@ -115,4 +235,4 @@ joj::GeoSphere::GeoSphere(f32 radius, u32 subdivisions, Vector4 color)
         vertices[i].color = color;
     }
 }
-
+#endif
