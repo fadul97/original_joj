2e2399c514f1f76bb6293a317f0779d3be7cc0ae
Leonardo Fadul <lsffadul@gmail.com>
2024-03-17 17:08:42 -0300

    WIP: DX11VertexBuffer class

---
+++ src/renderer/dx11/vertex_buffer_dx11.cpp
@@ -0,0 +1,32 @@
+#include "renderer/dx11/vertex_buffer_dx11.h"
+
+joj::DX11VertexBuffer::DX11VertexBuffer()
+{
+    m_vbd = { 0 };
+    m_vertex_buffer = nullptr;
+    m_init_data = { 0 };
+}
+
+joj::DX11VertexBuffer::~DX11VertexBuffer()
+{
+    // Release vertex buffer
+    if (m_vertex_buffer)
+    {
+        m_vertex_buffer->Release();
+        m_vertex_buffer = nullptr;
+    }
+}
+
+void joj::DX11VertexBuffer::build(u32 byte_width, const void* data)
+{
+    // Buffer to store vertices, describe how data will be accessed and where it will be bound to the rendering pipeline
+    m_vbd.Usage = D3D11_USAGE_IMMUTABLE;
+    m_vbd.ByteWidth = byte_width;
+    m_vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+    m_vbd.CPUAccessFlags = 0;
+    m_vbd.MiscFlags = 0;
+    m_vbd.StructureByteStride = 0;
+
+    // Specify data we want to initialize the buffer contents with
+    m_init_data.pSysMem = data;
+}
