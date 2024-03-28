21f8ae45d6a116ca89e7062986243f4bf56ba286
Leonardo Fadul <lsffadul@gmail.com>
2024-03-28 01:39:12 -0300

    Refactored PlatformManager

--- include/joj/platform/platform_manager.h
+++ include/joj/platform/platform_manager.h
@@ -7,6 +7,7 @@
 #include <string>
 #include "window.h"
 #include "input.h"
+#include "timer.h"
 #include "graphics/context.h"
 #include <memory>
 #include "icon_image.h"
@@ -14,56 +15,30 @@
 
 namespace joj
 {
-    template<class Twindow>
+    enum class BackendRenderer { DX11, GL };
+
+    template<class Twindow, class Tinput>
     class JAPI IPlatformManager
     {
     public:
         IPlatformManager();
         virtual ~IPlatformManager();
 
-        virtual b8 is_running() const = 0;
+        virtual std::unique_ptr<Twindow> create_window(i16 width, i16 height, const char* title, WindowMode mode) = 0;
+        virtual std::unique_ptr<Twindow> create_simple_window(i16 width, i16 height, const char* title) = 0;
 
-        virtual b8 is_key_down(u32 key) const = 0;
-        virtual b8 is_key_pressed(u32 key) const = 0;
-        virtual b8 is_key_up(u32 key) const = 0;
+        virtual std::unique_ptr<Tinput> create_input() = 0;
 
-        virtual b8 is_button_down(Buttons button) const = 0;
-        virtual b8 is_button_up(Buttons button) const = 0;
+        virtual std::unique_ptr<GraphicsContext<Twindow>> create_context(std::unique_ptr<Twindow>& window, BackendRenderer backend_renderer) = 0;
+        virtual ErrorCode create_window_and_context(std::unique_ptr<Twindow>& window, BackendRenderer backend_renderer) = 0;
 
-        virtual i16 get_xmouse() const = 0;
-        virtual i16 get_ymouse() const = 0;
-        virtual i16 get_mouse_wheel() const = 0;
+        virtual std::unique_ptr<Timer> create_timer() = 0;
 
-        virtual void hide_cursor(b8 hide) = 0;
-
-        virtual std::unique_ptr<Twindow>& get_window() = 0;
-
-        virtual void set_window_mode(WindowMode mode) = 0;
-        virtual void set_window_color(i32 r, i32 g, i32 b) = 0;
         virtual void set_on_focus(void(*func)()) = 0;
         virtual void set_lost_focus(void(*func)()) = 0;
-        virtual void close_window() = 0;
 
-        virtual ErrorCode init(i16 width, i16 height, std::string title, WindowMode mode) = 0;
-        virtual ErrorCode create_window() = 0;
-        virtual ErrorCode create_simple_window(i16 width, i16 height, std::string title, WindowMode mode) = 0;
-        virtual ErrorCode create_context(BackendRenderer backend_renderer) = 0;
-        virtual ErrorCode create_window_and_context(BackendRenderer backend_renderer) = 0;
-        virtual b8 process_events() = 0;
-        virtual void swap_buffers() = 0;
-        virtual void shutdown() = 0;
-
-        virtual f32 get_aspect_ratio() const = 0;
-        virtual f32 get_xcenter() const = 0;
-        virtual f32 get_ycenter() const = 0;
-
-        virtual void begin_period() = 0;
-        virtual void end_period() = 0;
-        virtual void start_timer() = 0;
-        virtual void stop_timer() = 0;
-        virtual f32 reset_timer() = 0;
-
-        virtual void set_window_icon(i32 count, IconImage& image) = 0;
+        virtual void set_window_icon(std::unique_ptr<Twindow>& window, i32 count, IconImage& image) = 0;
+        virtual void hide_cursor(b8 hide) = 0;
     };
 } // namespace joj
 
--- src/platform/platform_manager.cpp
+++ src/platform/platform_manager.cpp
@@ -4,20 +4,21 @@
 #include "platform/x11/window_x11.h"
 #elif JPLATFORM_WINDOWS
 #include "platform/win32/window_win32.h"
+#include "platform/win32/input_win32.h"
 #endif // JPLATFORM_WINDOWS
 
-template<class Twindow>
-joj::IPlatformManager<Twindow>::IPlatformManager()
+template<class Twindow, class Tinput>
+joj::IPlatformManager<Twindow, Tinput>::IPlatformManager()
 {
 }
 
-template<class Twindow>
-joj::IPlatformManager<Twindow>::~IPlatformManager()
+template<class Twindow, class Tinput>
+joj::IPlatformManager<Twindow, Tinput>::~IPlatformManager()
 {
 }
 
 #if JPLATFORM_LINUX
 template class joj::IPlatformManager<joj::X11Window>;
 #elif JPLATFORM_WINDOWS
-template class joj::IPlatformManager<joj::Win32Window>;
+template class joj::IPlatformManager<joj::Win32Window, joj::Win32Input>;
 #endif // JPLATFORM_WINDOWS
