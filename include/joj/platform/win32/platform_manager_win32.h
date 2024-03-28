2d4b79789296e67b9ed5feadb3ee2c1abafbe3c1
Leonardo Fadul <lsffadul@gmail.com>
2024-03-28 01:39:49 -0300

    Refactored Win32PlatformManager

--- include/joj/platform/win32/platform_manager_win32.h
+++ include/joj/platform/win32/platform_manager_win32.h
@@ -15,144 +15,37 @@
 
 namespace joj
 {
-    class JAPI Win32PlatformManager : public IPlatformManager<Win32Window>
+    class JAPI Win32PlatformManager : public IPlatformManager<Win32Window, Win32Input>
     {
     public:
         Win32PlatformManager();
         ~Win32PlatformManager();
 
-        b8 is_running() const override;
+        std::unique_ptr<Win32Window> create_window(i16 width, i16 height, const char* title, WindowMode mode) override;
+        std::unique_ptr<Win32Window> create_simple_window(i16 width, i16 height, const char* title) override;
 
-        b8 is_key_down(u32 key) const override;
-        b8 is_key_pressed(u32 key) const override;
-        b8 is_key_up(u32 key) const override;
+        std::unique_ptr<GraphicsContext<Win32Window>> create_context(std::unique_ptr<Win32Window>& window, BackendRenderer backend_renderer) override;
+        ErrorCode create_window_and_context(std::unique_ptr<Win32Window>& window, BackendRenderer backend_renderer) override;
+        
+        std::unique_ptr<Win32Input> create_input() override;
 
-        b8 is_button_down(Buttons button) const override;
-        b8 is_button_up(Buttons button) const override;
+        std::unique_ptr<Timer> create_timer() override;
 
-        i16 get_xmouse() const override;
-        i16 get_ymouse() const override;
-        i16 get_mouse_wheel() const override;
-
-        void hide_cursor(b8 hide) override;
-
-        void set_window_mode(WindowMode mode) override;
-        void set_window_color(i32 r, i32 g, i32 b) override;
         void set_on_focus(void(*func)()) override;
         void set_lost_focus(void(*func)()) override;
 
-        std::unique_ptr<Win32Window>& get_window() override;
-
-        void close_window() override;
-
-        ErrorCode init(i16 width, i16 height, std::string title, WindowMode mode) override;
-        ErrorCode create_window() override;
-        ErrorCode create_simple_window(i16 width, i16 height, std::string title, WindowMode mode) override;
-        ErrorCode create_context(BackendRenderer backend_renderer) override;
-        ErrorCode create_window_and_context(BackendRenderer backend_renderer) override;
-        b8 process_events() override;
-        void swap_buffers() override;
-        void shutdown() override;
-
-        f32 get_aspect_ratio() const override;
-        f32 get_xcenter() const override;
-        f32 get_ycenter() const override;
-
-        void begin_period() override;
-        void end_period() override;
-        void start_timer() override;
-        void stop_timer() override;
-        f32 reset_timer() override;
-
-        void set_window_icon(i32 count, IconImage& image) override;
-
-    private:
-        MSG msg;
-        std::unique_ptr<Win32Window> m_window;
-        std::unique_ptr<Win32Input> m_input;
-        std::unique_ptr<DX11Context> m_dx11_context;
-        std::unique_ptr<Win32GLContext> m_gl_context;
-        std::unique_ptr<Win32Timer> m_timer;
+        void set_window_icon(std::unique_ptr<Win32Window>& window, i32 count, IconImage& image) override;
+        void hide_cursor(b8 hide) override;
     };
-
-    inline std::unique_ptr<Win32Window>& Win32PlatformManager::get_window()
-	{ return m_window; }
-    
-    inline b8 Win32PlatformManager::is_running() const
-	{ return (m_window->is_running() && msg.message != WM_QUIT); }
-
-    inline b8 Win32PlatformManager::is_key_down(u32 key) const
-    { return m_input->is_key_down(key); }
-
-    inline b8 Win32PlatformManager::is_key_pressed(u32 key) const
-    { return m_input->is_key_pressed(key); }
-
-    inline b8 Win32PlatformManager::is_key_up(u32 key) const
-    { return m_input->is_key_up(key); }
-
-    inline b8 Win32PlatformManager::is_button_down(Buttons button) const
-    { return m_input->is_button_down(button); }
-
-    inline b8 Win32PlatformManager::is_button_up(Buttons button) const
-    { return m_input->is_button_up(button); }
-
-    inline i16 Win32PlatformManager::get_xmouse() const
-    { return m_input->get_xmouse(); }
-
-    inline i16 Win32PlatformManager::get_ymouse() const
-    { return m_input->get_ymouse(); }
-
-    inline i16 Win32PlatformManager::get_mouse_wheel() const
-    { return m_input->get_mouse_wheel(); }
 
     inline void Win32PlatformManager::hide_cursor(b8 hide)
     { ShowCursor(!hide); }
 
-    inline void Win32PlatformManager::set_window_mode(WindowMode mode)
-    { m_window->set_mode(mode); }
+    inline void Win32PlatformManager::set_on_focus(void(*func)())
+    { printf("TODO()!\n"); }
 
-    inline void Win32PlatformManager::set_window_color(i32 r, i32 g, i32 b)
-    { m_window->set_color(r, g, b); }
-
-    // Set function to be executed when wubdiw regains focus
-	inline void Win32PlatformManager::set_on_focus(void(*func)())
-	{ m_window->set_on_focus(func); }
-
-	// Set function to be executed when wubdiw loses focus
-	inline void Win32PlatformManager::set_lost_focus(void(*func)())
-	{ m_window->set_lost_focus(func); }
-
-    inline void Win32PlatformManager::close_window()
-    { m_window->close(); }
-
-    inline f32 Win32PlatformManager::get_aspect_ratio() const
-    { return m_window->get_aspect_ratio(); }
-
-    inline f32 Win32PlatformManager::get_xcenter() const
-    { return m_window->get_xcenter(); }
-
-    inline f32 Win32PlatformManager::get_ycenter() const
-    { return m_window->get_ycenter(); }
-
-    // Adjust sleep resolution to 1 millisecond
-	inline void Win32PlatformManager::begin_period()
-	{ m_timer->time_begin_period(); }
-
-	// Return sleep resolution to original value
-	inline void Win32PlatformManager::end_period()
-	{ m_timer->time_end_period(); }
-
-	// Start/resume counting time
-	inline void Win32PlatformManager::start_timer()
-	{ m_timer->start(); }
-
-	// Stop counting time
-	inline void Win32PlatformManager::stop_timer()
-	{ m_timer->stop(); }
-
-	// Restarts timer counter and returns elapsed time
-	inline f32 Win32PlatformManager::reset_timer()
-	{ return m_timer->reset(); }
+    inline void Win32PlatformManager::set_lost_focus(void(*func)())
+    { printf("TODO()!\n"); }
 } // namespace joj
 
 #endif // JOJ_WIN32_PLATFORM_MANAGER_H
--- src/platform/win32/platform_manager_win32.cpp
+++ src/platform/win32/platform_manager_win32.cpp
@@ -4,128 +4,114 @@
 
 joj::Win32PlatformManager::Win32PlatformManager()
 {
-    m_window = std::make_unique<Win32Window>();
-    m_timer = std::make_unique<Win32Timer>();
-
-    // ATTENTION: input must be initialized after window creation
-    m_input = nullptr;
-    
-    m_dx11_context = nullptr;
-    m_gl_context = nullptr;
-    
-    msg = { 0 };
 }
 
 joj::Win32PlatformManager::~Win32PlatformManager()
 {
 }
 
-joj::ErrorCode joj::Win32PlatformManager::init(i16 width, i16 height, std::string title, WindowMode mode)
+std::unique_ptr<joj::Win32Window> joj::Win32PlatformManager::create_window(i16 width, i16 height, const char* title, WindowMode mode)
 {
-    m_window = std::make_unique<Win32Window>(width, height, title);
-    
-    m_window->set_mode(mode);
-    m_window->set_color(60, 60, 60);
+    auto window = std::make_unique<Win32Window>(width, height, title);
+    window->set_mode(mode);
+    window->set_color(60, 60, 60);
 
-    return ErrorCode::OK;
-}
-
-joj::ErrorCode joj:: Win32PlatformManager::create_window()
-{
-    if (!m_window->create())
+    if (!window->create())
     {
-        // TODO: use own logger and return value, cleanup?
-        printf("Failed to initialize Win32Window.\n");
-        return ErrorCode::ERR_WINDOW_CREATION;
+        // TODO: Use own logger
+        printf("Failed to create window.\n");
+        return nullptr;
     }
-    
-    // ATTENTION: input must be initialized after window creation
-    m_input = std::make_unique<Win32Input>();
 
-    return ErrorCode::OK;
+    return window;
 }
 
-joj::ErrorCode joj::Win32PlatformManager::create_simple_window(i16 width, i16 height, std::string title, WindowMode mode)
+std::unique_ptr<joj::Win32Window> joj::Win32PlatformManager::create_simple_window(i16 width, i16 height, const char* title)
 {
-    // TODO: use own logger
-    printf("TODO()!\n");
-    return ErrorCode::FAILED;
+    auto window = std::make_unique<Win32Window>(width, height, title);
+
+    if (!window->create())
+    {
+        // TODO: Use own logger
+        printf("Failed to create window.\n");
+        return nullptr;
+    }
+
+    return window;
 }
 
-joj::ErrorCode joj::Win32PlatformManager::create_context(BackendRenderer backend_renderer)
+std::unique_ptr<joj::Win32Input> joj::Win32PlatformManager::create_input()
+{
+    auto input = std::make_unique<Win32Input>();
+
+    if (input == nullptr)
+    {
+        // TODO: Use own logger
+        printf("Failed to create input.\n");
+        return nullptr;
+    }
+
+    return input;
+}
+
+std::unique_ptr<joj::Timer> joj::Win32PlatformManager::create_timer()
+{
+    auto timer = std::make_unique<Win32Timer>();
+
+    if (timer == nullptr)
+    {
+        // TODO: Use own logger
+        printf("Failed to create timer.\n");
+        return nullptr;
+    }
+
+    return timer;
+}
+
+std::unique_ptr<joj::GraphicsContext<joj::Win32Window>> joj::Win32PlatformManager::create_context(std::unique_ptr<Win32Window>& window, BackendRenderer backend_renderer)
 {
     switch (backend_renderer)
     {
     case BackendRenderer::GL:
-        m_gl_context = std::make_unique<Win32GLContext>();
-    
-        if (!m_gl_context->create(m_window))
+    {
+        auto context = std::make_unique<Win32GLContext>();
+
+        if (!context->create(window))
         {
             // TODO: use own logger, cleanup?
-            printf("Failed to initialize GLContext.\n");
-            return ErrorCode::ERR_CONTEXT_CREATION;
+            printf("Failed to create GLContext.\n");
+            return nullptr;
         }
 
-        // FIXME: Check make_current method
-        m_gl_context->make_current(m_window);
+        context->make_current(window);
 
-        return ErrorCode::OK;
-
-    case BackendRenderer::DX11:
-        m_dx11_context = std::make_unique<DX11Context>();
-        
-        if (!m_dx11_context->create(m_window))
-        {
-            // TODO: use own logger
-            printf("Failed to initialize DX11Context.\n");
-            return ErrorCode::ERR_CONTEXT_CREATION;
-        }
-
-        // FIXME: Check make_current method
-        m_dx11_context->make_current(m_window);
-        
-        return ErrorCode::OK;
+        return context;
     }
 
-    return ErrorCode::FAILED;
+    case BackendRenderer::DX11:
+        auto context = std::make_unique<DX11Context>();
+        
+        if (!context->create(window))
+        {
+            // TODO: use own logger, cleanup?
+            printf("Failed to create DX11Context.\n");
+            return nullptr;
+        }
+
+        return context;
+    }
+
+    return nullptr;
 }
 
-joj::ErrorCode joj::Win32PlatformManager::create_window_and_context(BackendRenderer backend_renderer)
+joj::ErrorCode joj::Win32PlatformManager::create_window_and_context(std::unique_ptr<Win32Window>& window, BackendRenderer backend_renderer)
 {
     // TODO: use own logger
     printf("TODO()!\n");
     return ErrorCode::ERR_PLATFORM_MANAGER_CREATION;
 }
 
-b8 joj::Win32PlatformManager::process_events()
-{
-    if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-        return true;
-    }
-
-    return false;
-}
-
-void joj::Win32PlatformManager::swap_buffers()
-{
-    SwapBuffers(m_window->get_device_context());
-}
-
-void joj::Win32PlatformManager::shutdown()
-{
-    if (m_gl_context)
-        m_gl_context->destroy();
-
-    if (m_dx11_context)
-        m_dx11_context->destroy();
-
-    m_window->shutdown();
-}
-
-void joj::Win32PlatformManager::set_window_icon(i32 count, IconImage& image)
+void joj::Win32PlatformManager::set_window_icon(std::unique_ptr<Win32Window>& window, i32 count, IconImage& image)
 {
     // TODO: use own logger
     printf("TODO()!\n");
