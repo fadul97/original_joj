028119e48733ace5a1c06842a24d5d3fdc965b09
Leonardo Fadul <lsffadul@gmail.com>
2024-05-03 20:44:25 -0300

    Finished to refactor Window class (for now)

--- include/joj/platform/window.h
+++ include/joj/platform/window.h
@@ -4,7 +4,7 @@
 #define JOJ_ENGINE_IMPLEMENTATION
 #include "defines.h"
 
-#include "error_codes.h"
+#include "error_code.h"
 
 namespace joj
 {
@@ -14,26 +14,28 @@ namespace joj
     class JAPI Window
     {
     public:
-        Window(i16 width, i16 height, const char* title, WindowMode mode);
+        Window();
         virtual ~Window();
 
-        TWindowConfig& get_window_config() const;
+        TWindowConfig& get_window_config();
 
-        [[nodiscard]] i16 get_width() const;
-        [[nodiscard]] i16 get_height() const;
+        virtual void get_window_size(u16& width, u16& height) = 0;
+        virtual void get_client_size(u16& width, u16& height) = 0;
+
+        [[nodiscard]] virtual u16 get_width() const = 0;
+        [[nodiscard]] virtual u16 get_height() const = 0;
         [[nodiscard]] const char* get_title() const;
-        [[nodiscard]] i16 get_xcenter() const;
-        [[nodiscard]] i16 get_ycenter() const;
-        [[nodiscard]] f32 get_aspect_ratio() const;
+        [[nodiscard]] virtual u16 get_xcenter() = 0;
+        [[nodiscard]] virtual u16 get_ycenter() = 0;
+        [[nodiscard]] virtual f32 get_aspect_ratio() const = 0;
         [[nodiscard]] b8 is_running() const;
-        [[nodiscard]] WindowMode get_mode() const;
+
+        void stop_running();
         
         virtual void set_mode(WindowMode mode) = 0;
         virtual void set_color(u32 r, u32 g, u32 b) = 0;
         
-        virtual ErrorCode init() = 0;
-        virtual ErrorCode create() = 0;
-        virtual void show() = 0;
+        virtual ErrorCode create(i16 width, i16 height, const char* title, WindowMode mode) = 0;
         virtual void destroy() = 0;
         virtual void swap_buffers() = 0;
 
@@ -41,49 +43,26 @@ namespace joj
         virtual void set_lost_focus(void(*func)()) = 0;
 
     protected:
-        i16 m_width;
-        i16 m_height;
         const char* m_title;
         b8 m_running;
-        WindowMode m_mode;
         TWindowConfig m_window_config;
     };
 
     template<typename TWindowConfig>
-    inline TWindowConfig& Window<TWindowConfig>::get_window_config() const
+    inline TWindowConfig& Window<TWindowConfig>::get_window_config()
     { return m_window_config; }
 
-    template<typename TWindowConfig>
-    inline i16 Window<TWindowConfig>::get_width() const
-    { return m_width; }
-
-    template<typename TWindowConfig>
-    inline i16 Window<TWindowConfig>::get_height() const
-    { return m_height; }
-    
     template<typename TWindowConfig>
     inline const char* Window<TWindowConfig>::get_title() const
     { return m_title; }
     
     template<typename TWindowConfig>
-    inline i16 Window<TWindowConfig>::get_xcenter() const
-    { return static_cast<i16>(m_width / 2); }
-    
-    template<typename TWindowConfig>
-    inline i16 Window<TWindowConfig>::get_ycenter() const
-    { return static_cast<i16>(m_height / 2); }
-    
-    template<typename TWindowConfig>
-    inline f32 Window<TWindowConfig>::get_aspect_ratio() const
-    { return static_cast<f32>(m_width) / m_height ; }
-
-    template<typename TWindowConfig>
     inline b8 Window<TWindowConfig>::is_running() const
     { return m_running; }
 
     template<typename TWindowConfig>
-    inline WindowMode Window<TWindowConfig>::get_mode() const
-    { return m_mode; }
+    inline void Window<TWindowConfig>::stop_running()
+    { m_running = false; }
 }
 
 #endif // JOJ_WINDOW_H
--- src/platform/window.cpp
+++ src/platform/window.cpp
@@ -2,13 +2,11 @@
 
 #if JPLATFORM_LINUX
 #include "platform/x11/window_x11.h"
-#elif JPLATFORM_WINDOWS
-#include "platform/win32/window_win32.h"
 #endif
 
 template<typename TWindowConfig>
-joj::Window<TWindowConfig>::Window(const i16 width, const i16 height, const char* title, const WindowMode mode)
-    : m_width(width), m_height(height), m_title(title), m_running(false), m_mode(mode)
+joj::Window<TWindowConfig>::Window()
+    : m_title("Abstract Window"), m_running(false), m_window_config()
 {
 }
 
@@ -18,5 +16,6 @@ joj::Window<TWindowConfig>::~Window() = default;
 #if JPLATFORM_LINUX
 template class joj::Window<u32>;
 #elif JPLATFORM_WINDOWS
-template class joj::Window<HWND>;
+#include "platform/win32/window_win32.h"
+template class joj::Window<joj::WindowConfig>;
 #endif
