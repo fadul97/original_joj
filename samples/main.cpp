ca060688c4110ed394c9c9516dcbabcd2c54a280
Leonardo Fadul <lsffadul@gmail.com>
2024-05-03 20:45:20 -0300

    Example using the new Win32Window class

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,10 +1,46 @@
 #include <cstdio>
-
 #include "joj/platform/win32/window_win32.h"
+
+b8 process_events();
 
 int main()
 {
     joj::Win32Window window{};
+    if (window.create(400, 400, "Refactored window", joj::WindowMode::Windowed) < joj::ErrorCode::OK) {
+        return -1;
+    }
+
+    u16 width = 0;
+    u16 height = 0;
+    window.get_window_size(width, height);
+    printf("Window size: %dx%d\n", width, height);
+
+    window.get_client_size(width, height);
+    printf("Client size: %dx%d\n", width, height);
+
+    while (window.is_running()) {
+        if (!process_events()) {
+            window.stop_running();
+        }
+
+    }
+
+    window.destroy();
+
     printf("Hello, Refactoring!\n");
     return 0;
+}
+
+b8 process_events() {
+    MSG msg{};
+
+    if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+
+        if (msg.message == WM_QUIT || msg.message == WM_DESTROY)
+            return false;
+    }
+
+    return true;
 }
