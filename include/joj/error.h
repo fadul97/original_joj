aa426e662d644300f7c49c91ee835c9ef370273d
Leonardo Fadul <lsffadul@gmail.com>
2024-05-03 20:40:30 -0300

    Fixed duplicate name for ErrorCode

--- include/joj/error.h
+++ include/joj/error.h
@@ -5,63 +5,10 @@
 #include "defines.h"
 
 #include <string>
+#include "error_code.h"
 
 namespace joj
 {
-    enum class ErrorCode
-    {
-        UNKNOWN,
-        OK,
-        FAILED,
-
-        // General Engine error codes
-        ERR_ENGINE_INIT,
-
-        // General PlatformManager error codes
-        ERR_PLATFORM_MANAGER_INIT,
-        ERR_PLATFORM_MANAGER_CREATION,
-        
-        // General Window error codes
-        ERR_WINDOW_INIT,
-        ERR_WINDOW_CREATION,
-        ERR_WINDOW_ID_CREATION,
-
-        // General Input error codes
-        ERR_INPUT_CREATION,
-
-        // General Timer error codes
-        ERR_TIMER_CREATION,
-
-        // X11Window error codes
-        ERR_WINDOW_DISPLAY,
-
-        // TODO: Win32Window error codes
-
-        // General Renderer error codes
-        ERR_RENDERER_INIT,
-        ERR_RENDERER_PIPELINE_ERROR,
-        
-        // General GraphicsContext error codes
-        ERR_CONTEXT_INIT,
-        ERR_CONTEXT_CREATION,
-        
-        // X11GLContext error codes
-        ERR_CONTEXT_GLX_VERSION,
-        ERR_CONTEXT_FB_UNMATCH,
-        ERR_CONTEXT_VISUAL_CREATION,
-        ERR_CONTEXT_SCREEN_UNMATCH,
-        ERR_CONTEXT_EXTENSION_STR,
-
-        // X11Renderer error codes
-        ERR_RENDERER_SWAPCHAIN_CREATION,
-        ERR_RENDERER_SWAPCHAIN_GET_BUFFER,
-        ERR_RENDER_TARGET_VIEW_CREATION,
-        ERR_RENDERER_TEXTURE2D_CREATION,
-        ERR_RENDERER_DEPTHSTENCIL_VIEW_CREATION,
-        ERR_RENDERER_BLEND_STATE_CREATION,
-        ERR_RENDERER_RASTERIZER_CREATION
-    };
-
     void print_error(ErrorCode e, const std::string& func, const std::string& file, i32 line);
 
     const char* error_to_string(ErrorCode e);
