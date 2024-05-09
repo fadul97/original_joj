717498d4f0f5276d9797a2637aba7a1f9ef05175
Leonardo Fadul <lsffadul@gmail.com>
2024-05-08 22:50:35 -0300

    Helper functions for ErrorCode

--- include/joj/error_code.h
+++ include/joj/error_code.h
@@ -4,62 +4,19 @@
 #define JOJ_ENGINE_IMPLEMENTATION
 #include "defines.h"
 
-namespace joj
-{
-    enum class ErrorCode : i32
-    {
-        WARNING = 1,
+namespace  joj {
+    enum class ErrorCode : i32 {
         OK = 0,
-        UNKNOWN = -1,
-        FAILED = -2,
-
-        // General Engine error codes
-        ERR_ENGINE_INIT,
-
-        // General PlatformManager error codes
-        ERR_PLATFORM_MANAGER_INIT,
-        ERR_PLATFORM_MANAGER_CREATION,
-
-        // General Window error codes
-        ERR_WINDOW_INIT,
-        ERR_WINDOW_CREATION,
-        ERR_WINDOW_ID_CREATION,
-
-        // General Input error codes
-        ERR_INPUT_CREATION,
-
-        // General Timer error codes
-        ERR_TIMER_CREATION,
-
-        // X11Window error codes
-        ERR_WINDOW_DISPLAY,
-
-        // TODO: Win32Window error codes
-
-        // General Renderer error codes
-        ERR_RENDERER_INIT,
-        ERR_RENDERER_PIPELINE_ERROR,
-
-        // General GraphicsContext error codes
-        ERR_CONTEXT_INIT,
-        ERR_CONTEXT_CREATION,
-
-        // X11GLContext error codes
-        ERR_CONTEXT_GLX_VERSION,
-        ERR_CONTEXT_FB_UNMATCH,
-        ERR_CONTEXT_VISUAL_CREATION,
-        ERR_CONTEXT_SCREEN_UNMATCH,
-        ERR_CONTEXT_EXTENSION_STR,
-
-        // X11Renderer error codes
-        ERR_RENDERER_SWAPCHAIN_CREATION,
-        ERR_RENDERER_SWAPCHAIN_GET_BUFFER,
-        ERR_RENDER_TARGET_VIEW_CREATION,
-        ERR_RENDERER_TEXTURE2D_CREATION,
-        ERR_RENDERER_DEPTHSTENCIL_VIEW_CREATION,
-        ERR_RENDERER_BLEND_STATE_CREATION,
-        ERR_RENDERER_RASTERIZER_CREATION
+        ERR_WINDOW = -1,
+        ERR_CONTEXT = -2,
+        ERR_FILE_OPEN = -3,
+        ERR_VSHADER = -4,
+        ERR_FSHADER = -5,
+        ERR_SHADER_PROGRAM = -6
     };
+
+    i32 err_to_i32(ErrorCode err);
+    const char* err_to_str(ErrorCode err);
 }
 
 #endif // JOJ_ERROR_CODES_H
---
+++ src/error_code.cpp
@@ -0,0 +1,57 @@
+#include "error_code.h"
+
+i32 joj::err_to_i32(const ErrorCode err) {
+    switch (err) {
+        case ErrorCode::OK:
+            return 0;
+
+        case ErrorCode::ERR_WINDOW:
+            return -1;
+
+        case ErrorCode::ERR_CONTEXT:
+            return -2;
+
+        case ErrorCode::ERR_FILE_OPEN:
+            return -3;
+
+        case ErrorCode::ERR_VSHADER:
+            return -4;
+
+        case ErrorCode::ERR_FSHADER:
+            return -5;
+
+        case ErrorCode::ERR_SHADER_PROGRAM:
+            return -6;
+
+        default:
+            return 1;
+    }
+}
+
+const char* joj::err_to_str(const ErrorCode err) {
+    switch (err) {
+        case ErrorCode::OK:
+            return "OK";
+
+        case ErrorCode::ERR_WINDOW:
+            return "ERR_WINDOW";
+
+        case ErrorCode::ERR_CONTEXT:
+            return "ERR_CONTEXT";
+
+        case ErrorCode::ERR_FILE_OPEN:
+            return "ERR_FILE_OPEN";
+
+        case ErrorCode::ERR_VSHADER:
+            return "ERR_VSHADER";
+
+        case ErrorCode::ERR_FSHADER:
+            return "ERR_FSHADER";
+
+        case ErrorCode::ERR_SHADER_PROGRAM:
+            return "ERR_SHADER_PROGRAM";
+
+        default:
+            return "UNKNOWN";
+    }
+}
