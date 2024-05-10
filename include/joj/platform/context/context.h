2f78dbcbf7ed5420e9381580b74599d180ddaa6b
Leonardo Fadul <lsffadul@gmail.com>
2024-05-09 22:30:15 -0300

    Refactored GraphicsContext methods

--- include/joj/platform/context/context.h
+++ include/joj/platform/context/context.h
@@ -4,8 +4,7 @@
 #define JOJ_ENGINE_IMPLEMENTATION
 #include "defines.h"
 
-#include <memory>
-#include "platform/window.h"
+#include "error_code.h"
 
 namespace joj
 {
@@ -16,14 +15,14 @@ namespace joj
         GraphicsContext();
         virtual ~GraphicsContext();
 
-        virtual b8 create(std::unique_ptr<Twindow>& window) = 0;
-        virtual void make_current(std::unique_ptr<Twindow>& window) = 0;
+        // TODO: Change return value of create and make_current methods
+        virtual ErrorCode  create(Twindow& window) = 0;
+        virtual ErrorCode  make_current(Twindow& window) = 0;
         virtual void destroy() = 0;
           
     protected:
         virtual void log_hardware_info() = 0;
     };
 }
-
 
 #endif // JOJ_GRAPHICS_CONTEXT_H
--- src/platform/context/context.cpp
+++ src/platform/context/context.cpp
@@ -2,22 +2,17 @@
 
 #if JPLATFORM_LINUX
 #include "platform/x11/window_x11.h"
-#elif JPLATFORM_WINDOWS
-#include "platform/win32/window_win32.h"
-#endif // JPLATFORM_WINDOWS
+#endif // JPLATFORM_LINUX
 
 template<class Twindow>
-joj::GraphicsContext<Twindow>::GraphicsContext()
-{
-}
+joj::GraphicsContext<Twindow>::GraphicsContext() = default;
 
 template<class Twindow>
-joj::GraphicsContext<Twindow>::~GraphicsContext()
-{
-}
+joj::GraphicsContext<Twindow>::~GraphicsContext() = default;
 
 #if JPLATFORM_LINUX
 template class joj::GraphicsContext<joj::X11Window>;
 #elif JPLATFORM_WINDOWS
-template class joj::GraphicsContext<joj::Win32Window>;
+#include "platform/win32/window_win32.h"
+template class joj::GraphicsContext<joj::WindowConfig>;
 #endif // JPLATFORM_WINDOWS
