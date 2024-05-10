8a52d58067c960b6de71e93dedd97bb6f5679acc
Leonardo Fadul <lsffadul@gmail.com>
2024-05-09 22:18:41 -0300

    Refactored ErrorCode helper functions

--- src/error.cpp
+++ src/error.cpp
@@ -8,30 +8,7 @@ const char* joj::error_to_string(joj::ErrorCode e)
     switch (e)
     {
         case ErrorCode::OK: return "OK";
-        case ErrorCode::FAILED: return "FAILED";
-        case ErrorCode::ERR_ENGINE_INIT: return "ERR_ENGINE_INIT";
-        // case ErrorCode::ERR_PLATFORM_MANAGER_INIT: return "ERR_PLATFORM_MANAGER_INIT";
-        case ErrorCode::ERR_PLATFORM_MANAGER_CREATION: return "ERR_PLATFORM_MANAGER_CREATION";
-        case ErrorCode::ERR_WINDOW_INIT: return "ERR_WINDOW_INIT";
-        case ErrorCode::ERR_WINDOW_CREATION: return "ERR_WINDOW_CREATION";
-        case ErrorCode::ERR_WINDOW_ID_CREATION: return "ERR_WINDOW_ID_CREATION";
-        case ErrorCode::ERR_WINDOW_DISPLAY: return "ERR_WINDOW_DISPLAY";
-        case ErrorCode::ERR_RENDERER_INIT: return "ERR_RENDERER_INIT";
-        case ErrorCode::ERR_RENDERER_PIPELINE_ERROR: return "ERR_RENDERER_PIPELINE_ERROR";
-        case ErrorCode::ERR_CONTEXT_INIT: return "ERR_CONTEXT_INIT";
-        case ErrorCode::ERR_CONTEXT_CREATION: return "ERR_CONTEXT_CREATION";
-        case ErrorCode::ERR_CONTEXT_GLX_VERSION: return "ERR_CONTEXT_GLX_VERSION";
-        case ErrorCode::ERR_CONTEXT_FB_UNMATCH: return "ERR_CONTEXT_FB_UNMATCH";
-        case ErrorCode::ERR_CONTEXT_VISUAL_CREATION: return "ERR_CONTEXT_VISUAL_CREATION";
-        case ErrorCode::ERR_CONTEXT_SCREEN_UNMATCH: return "ERR_CONTEXT_SCREEN_UNMATCH";
-        case ErrorCode::ERR_CONTEXT_EXTENSION_STR: return "ERR_CONTEXT_EXTENSION_STR";
-        case ErrorCode::ERR_RENDERER_SWAPCHAIN_CREATION: return "ERR_RENDERER_SWAPCHAIN_CREATION";
-        case ErrorCode::ERR_RENDERER_SWAPCHAIN_GET_BUFFER: return "ERR_RENDERER_SWAPCHAIN_GET_BUFFER";
-        case ErrorCode::ERR_RENDER_TARGET_VIEW_CREATION: return "ERR_RENDER_TARGET_VIEW_CREATION";
-        case ErrorCode::ERR_RENDERER_TEXTURE2D_CREATION: return "ERR_RENDERER_TEXTURE2D_CREATION";
-        case ErrorCode::ERR_RENDERER_DEPTHSTENCIL_VIEW_CREATION: return "ERR_RENDERER_DEPTHSTENCIL_VIEW_CREATION";
-        case ErrorCode::ERR_RENDERER_BLEND_STATE_CREATION: return "ERR_RENDERER_BLEND_STATE_CREATION";
-        case ErrorCode::ERR_RENDERER_RASTERIZER_CREATION: return "ERR_RENDERER_RASTERIZER_CREATION";
+        case ErrorCode::ERR_CONTEXT: return "ERR_CONTEXT";
         default: return "UNKNOWN";
     }
 }
